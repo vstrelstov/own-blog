4b2db5cedcbb6c84f999a506aeeab0ad4ff3b509
vstrelstov <streltcov.vasiliy.bpo1401@yandex.ru>
2021-09-25 18:29:40 +0300

    Implemented blog controller

--- backend/src/blog/blog.controller.ts
+++ backend/src/blog/blog.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body, Query, Put, Delete } from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+    constructor(private blogService: BlogService) { }
+
+    @Get('posts')
+    async getPosts(@Res() res) {
+        const posts = await this.blogService.getPosts();
+        return res.status(HttpStatus.OK).json(posts);
+    }
+
+    @Get('post/:postID')
+    async getPost(@Res() res, @Param('postID', new ValidateObjectId()) postID) {
+        const post = await this.blogService.getPost(postID);
+        if (!post) throw new NotFoundException('Post does not exist!');
+        return res.status(HttpStatus.OK).json(post);
+
+    }
+
+    @Post('/post')
+    async addPost(@Res() res, @Body() createPostDto: CreatePostDto) {
+        const newPost = await this.blogService.addPost(createPostDto);
+        return res.status(HttpStatus.OK).json({
+            message: "Post has been submitted successfully!",
+            post: newPost
+        })
+    }
+
+    @Put('/edit')
+    async editPost(
+        @Res() res,
+        @Query('postID', new ValidateObjectId()) postID,
+        @Body() createPostDTO: CreatePostDto
+    ) {
+        const editedPost = await this.blogService.editPost(postID, createPostDTO);
+        if (!editedPost) throw new NotFoundException('Post does not exist!');
+        return res.status(HttpStatus.OK).json({
+            message: 'Post has been successfully updated',
+            post: editedPost
+        })
+    }
+
+
+    @Delete('/delete')
+    async deletePost(@Res() res, @Query('postID', new ValidateObjectId()) postID) {
+        const deletedPost = await this.blogService.deletePost(postID);
+        if (!deletedPost) throw new NotFoundException('Post does not exist!');
+        return res.status(HttpStatus.OK).json({
+            message: 'Post has been deleted!',
+            post: deletedPost
+        })
+    }
+}

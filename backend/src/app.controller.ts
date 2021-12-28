c1419426ffa80fd94512d3b6689b62778803e7ad
vstrelstov <streltcov.vasiliy.bpo1401@yandex.ru>
2021-12-28 23:45:10 +0300

    Created a class for auth guard

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Request, Post, UseGuards, Req } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -11,7 +11,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;

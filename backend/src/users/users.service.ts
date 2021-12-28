676aebbbcffef670adaea0ac51d660097ff27da6
vstrelstov <streltcov.vasiliy.bpo1401@yandex.ru>
2021-12-28 22:57:53 +0300

    WIP auth

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        
+    ];
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}

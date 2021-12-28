a10e94172afe30306ca95ac2c7d7ae037bb985df
vstrelstov <streltcov.vasiliy.bpo1401@yandex.ru>
2021-12-28 22:41:56 +0300

    Added auth and users stubs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/own-blog', { userNewUrlParser: true }),
-    BlogModule
+    BlogModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],

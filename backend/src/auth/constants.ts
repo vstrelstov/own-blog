a2e3462687b2744179be938f8271d968fa19168a
vstrelstov <streltcov.vasiliy.bpo1401@yandex.ru>
2022-01-09 19:34:08 +0300

    Added JWT for login route

---
+++ backend/src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'secretKey' // TODO: Protect this key using appropriate measures such as a secrets vault, environment variable, or configuration service. 
+};

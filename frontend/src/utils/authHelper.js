e1744c5d4997b0d3c0bbae41e847ca1db62881e9
vstrelstov <streltcov.vasiliy.bpo1401@yandex.ru>
2022-01-14 01:13:09 +0300

    Created authentication helper

---
+++ frontend/src/utils/authHelper.js
@@ -0,0 +1,12 @@
+export const authHelper = {
+    createAuthHeader() {
+        let token = sessionStorage.getItem(accessToken);
+        if (token) {
+            return {
+                headers: { Authorization: `Bearer ${token}` }
+            };
+        }
+
+        return undefined;
+    }
+}
